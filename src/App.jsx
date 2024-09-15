1261aed3a40380b394d6e91b4fff25a980a253f3
bomba-cat <bledion.morina@hotmail.com>
2024-09-15 12:58:40 +0200

    routers?:

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import NavBar from "./components/NavBar.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (<BrowserRouter>
+            <div>
+              <Routes>
+                <Route path="/" element={<NavBar />}>
+                  <Route index element={<h1>All Forums</h1>}>
+              </Routes>
+            </div>
             <p>Hi</p>
           </BrowserRouter>)
 }
