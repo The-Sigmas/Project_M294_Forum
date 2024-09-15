51bb92f214d50b2c5e4c1fa01386556fa729d629
bomba-cat <bledion.morina@hotmail.com>
2024-09-15 16:36:58 +0200

    yay

---
+++ src/components/CreateThread.jsx
@@ -0,0 +1,5 @@
+export default function CreateThread() {
+  return(<>
+    <p className="bg-gradient-to-r from-red-500 via-red-700 to-red-900 bg-clip-text text-transparent">Create a Thread</p>
+  </>)
+}
