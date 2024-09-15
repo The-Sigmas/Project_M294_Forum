055b580354392cd9af96d9adc21a14dd2c6702ab
bomba-cat <bledion.morina@hotmail.com>
2024-09-15 14:21:56 +0200

    category and a bit of navbar

---
+++ src/components/Category.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+function Category(props) {
+  return (
+    <Link className={`${props.s}`} to={`/${props.name.toLowerCase()}`}>
+      {props.name}
+    </Link>
+  );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,18 @@
 import { Outlet } from "react-router-dom";
+import Category from './Category.jsx'
+import '../App.css'
 
 export default function NavBar(){
+  const options = ["autos", "cpu", "gpu"]
+  const option_buttons = options.map(a =>
+    <Category s="m-8 p-4 font-mono text-6xl bg-teal-950" name={a} />
+  )
+
   return(<>
-    <h2>Silly<h2>
+    <div className="answer-buttons m-1 absolute bg-teal-600 top-4 -translate-y-1/2 -translate-x-1/2 left-4">
+      {option_buttons}
+    </div>
+ 
     <Outlet />
   </>)
 }
