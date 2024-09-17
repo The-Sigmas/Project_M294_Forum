37b4500a03f358ac2c0343fce4175b1e7f1fbc61
bomba-cat <bledion.morina@hotmail.com>
2024-09-17 09:03:35 +0200

    sigma

---
+++ src/components/ViewThread.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+import '../App.css'
+//Imports
+import Category from "./Category.jsx"
+
+export default function ViewThread( props ) {
+
+  return(<>
+  </>)
+}
