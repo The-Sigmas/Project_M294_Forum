684a58d9510bd9810f4e94eafe7a8efd1958eb21
bomba-cat <bledion.morina@hotmail.com>
2024-09-17 09:14:35 +0200

    closing

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
                         <Route path={`/threads/${ doc._id }`} element={<ViewThread id={`${doc._id}`}/>} />
                       ))
                     ) : (
-                        <Route path="/error" element={<About />}>
+                        <Route path="/error" element={<About />} />
                       )}
                 </Route>
               </Routes>
